559f9f6aa3a8b0bb49c89d8f5aecb43a88f1a361
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2024-09-04 10:11:01 -0700

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -10,15 +10,6 @@ from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-def split_data(df):
-    X = df.drop('Y', axis=1).values
-    y = df['Y'].values
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0)
-    data = {"train": {"X": X_train, "y": y_train},
-            "test": {"X": X_test, "y": y_test}}
-    return data
 
 # define functions
 def main(args):
@@ -45,6 +36,15 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    print("Splitting data...")
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness',
+    'SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+    return X_train, X_test, y_train, y_test
+
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
